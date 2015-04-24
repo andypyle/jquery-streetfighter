ef4e70fbdb50003e1e128418f57ed0cbe40a485a
Andrew Pyle <andy.pyle@gmail.com>
2015-04-24 16:34:20 -0700

    Added hadouken sound to onMouseDown event.

--- js/app.js
+++ js/app.js
@@ -15,12 +15,26 @@ $(document).ready(function(){
 		ryu.still.show();
 	})
 	.mousedown(function(){
+		playHadouken();
 		ryu.ready.hide();
 		ryu.throwing.show();
-		ryu.hadouken.show();
+		ryu.hadouken.finish().show()
+			.animate({
+				'left':'1020px'
+			}, 500,
+			function() {
+				$(this).hide();
+				$(this).css('left','520px');
+			})
 	})
 	.mouseup(function(){
 		ryu.ready.show();
 		ryu.throwing.hide();
 	});
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
